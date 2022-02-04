fd89682c34d4f25f063b194cf27289399201cedf
Joshua <jpri6587@sydney.edu.au>
2022-02-05 08:35:00 +1100

    updated flagging functions and makedspec

--- functions.sh
+++ functions.sh
@@ -5,6 +5,26 @@ function flag_extreme {
     uvflag vis=$vis select="amplitude($amp)" flagval=flag
 }
 
+function flag_channels {
+    vis=$1
+    band=$2
+    print "Flagging known bad channels in band $band"
+    case $band in
+	[2100]* )
+
+	    uvflag vis=$vis line=channel,60,435,1,1 flagval=flag
+	    uvflag vis=$vis line=channel,5,1300,1,1 flagval=flag
+
+	    ;;
+
+	[5500]* )
+	    ;;
+
+	[9000]* )
+	    ;;
+    esac
+}
+
 function print {
     msg=$1
     msgcolor="\e[38;5;166m"
