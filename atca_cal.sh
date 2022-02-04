7466b690a13a34ef2125bc4f124b44a7fdd10d6b
Joshua <jpri6587@sydney.edu.au>
2022-02-05 08:33:57 +1100

    updated prompts and flagging order

--- atca_cal.sh
+++ atca_cal.sh
@@ -1,37 +1,57 @@
 #!/bin/bash
 
+source functions.sh
+
 export proj_dir=$1
 export pcode=C3369
-export data_dir=/import/ada1/jpri6587/phd/atca/C3369/raw_data/OCTS2020/$proj_dir
+export altpcode=$2
+export process_dir=/import/ada1/jpri6587/phd/atca/$pcode
+export data_dir=$(find $process_dir/raw_data/*/ | grep $proj_dir$)
 
 # Load data
-while true; do
-    echo -e "\e[38;5;166mReload data? \e[38;5;196m(y/n)\e[39m"
-    read reload
-    case $reload in
-	
-	[Yy]* )
-	    echo -e "\e[38;5;166mFlushing atlod files and reloading.\e[39m"
-	    export reflag="y"
-	    export reautoflag="y"
-	    rm -rf $proj_dir/
-	    mkdir $proj_dir/
-	    cd $proj_dir
-	    atlod in=`ls $data_dir/*.$pcode | tr '\n' ','` out=$pcode.uv options=birdie,rfiflag,noauto,xycorr
-	    uvflag vis=$pcode.uv edge=40 flagval=flag
-	    uvsplit vis=$pcode.uv
-	    break;;
-	
-	[Nn]* )
-	    export reflag="n"
-	    echo -e "\e[38;5;166mSkipping data reload.\e[39m"
-	    cd $proj_dir
-	    break;;
-    esac
+prompt "Reload data?"
+read reload
+case $reload in
+
+    [Yy]* )
+	print "Flushing atlod files and reloading"
+	export reflag="y"
+
+	# Move pre-flagged primary scan back in
+	cp -r $proj_dir/*flagged_backup .
+	rm -rf $proj_dir/
+	mkdir $proj_dir/
+	cd $proj_dir
+
+	export infiles=$(ls $data_dir/* | grep $pcode | tr '\n' ',')
+
+	atlod in=$infiles out=$pcode.uv options=birdie,rfiflag,noauto,xycorr
+	uvflag vis=$pcode.uv edge=40 flagval=flag
+	uvsplit vis=$pcode.uv ;;
+
+    [Nn]* )
+	export reflag="n"
+	print "Skipping data reload"
+	cd $proj_dir ;;
+esac
+
+# Choose frequency
+export freqs=$(ls | grep 00 | awk -F . '{print $2"."$3}' | sort | uniq)
+print "Choose frequency / IF"
+select f in $freqs; do
+    if [[ "$REPLY" == stop ]]; then break; fi
+
+    if [[ "$REPLY" == "" ]]; then
+	print "'$REPLY' is not a valid choice"
+	continue
+    fi
+
+    export freq=$f
+    break
 done
 
 # Choose from files which sources to use as calibrators and target
-files=$(ls | grep 2100.1 | awk -F . '{print $1}')
+files=$(ls | grep $freq | awk -F . '{print $1}' | uniq)
 
 declare -a orders
 orders+=('pcal')
@@ -44,206 +64,233 @@ sources['scal']="secondary calibrator"
 sources['target']="science target"
 
 for src in "${orders[@]}"; do
-    echo -e "\n\e[38;5;166mChoose ${sources[$src]}\e[39m"
+    print "Choose ${sources[$src]}"
     select file in $files
     do
 	if [[ "$REPLY" == stop ]]; then break; fi
 
-	if [[ "$REPLY" == "" ]]
-	then
-	    echo -e "\e[38;5;196m'$REPLY' \e[38;5;166mis not a valid choice.\e[39m"
+	if [[ "$REPLY" == "" ]]; then
+	    print "'$REPLY' is not a valid choice"
 	    continue
 	fi
 
 	export $src=$file
-
 	break
+
     done
 done
 
-# --------
-# FLAGGING
-# --------
+# Restore pre-flagged versions
+for vis in $pcal $scal; do
+    cp -r ../$vis.$freq.flagged_backup $vis.$freq
+done
 
-if [[ $reflag != 'y' ]]; then
-    echo -e "\n\e[38;5;166mRe-do flagging? (y/n)\e[39m"
-    read reflag;
-else
-    case $reflag in
-	
-	[Yy]* )
-	    # Flag scan interrupted by wind stow
-	    uvflag vis=$target.2100* select="time(21:38:30,22:30:00)" flagval=flag
+# ------------------------------
+# PRIMARY / BANDPASS CALIBRATION
+# ------------------------------
 
-	    # Flag whole 4-5 baseline
-	    for vis in $pcal $scal $target; do
-		uvflag vis=$vis.2100* "select=antenna(4)(5)" flagval=flag;
-	    done
-	    
-	    # Initial automatic flagging
-	    if [[ $reautoflag != 'y' ]]; then
-		echo -e "\e[38;5;166mRe-run automatic flagging? (y/n)\n\e[39m"
-		read reautoflag;
-	    else 
-		case $reautoflag in
-		    [Yy]* )
-			for vis in $pcal.2100* $scal.2100* $target.2100*;
-			do echo -e "\n\e[38;5;166mRunning pgflag on $vis\e[39m"
-			   pgflag vis=$vis command="<b" device=/xs stokes=xx,yy,xy,yx
-			done
-			break;;
-		    [Nn]* )
-			break;;
-		esac
-	    fi
+prompt "Proceed with primary flagging?"
+read reflag
 
-	    # Iterate between bandpass solution and RFI flagging until spectrum is clean
-	    while true; do
+case $reflag in
 
-		# Solve for bandpass on primary calibrator
-		for i in 1 2;
-		do echo -e "\n\e[38;5;166mRunning mfcal on $i\e[39m"
-		   mfcal vis=$pcal.2100.$i interval=40.0 refant=1
-		done
+    [Yy]* )
 
-		# Run blflag to clean up lower level RFI 
-		for i in 1 2;
-		do echo -e "\n\e[38;5;166mRunning blflag on $pcal.2100.$i\e[39m"
-		   blflag vis=$pcal.2100.$i device=/xs stokes=xx,yy,xy,yx axis=chan,amp options=nofqav,nobase
-		   blflag vis=$pcal.2100.$i device=/xs stokes=xx,yy,xy,yx axis=time,amp options=nofqav
-		   blflag vis=$pcal.2100.$i device=/xs stokes=xx,yy,xy,yx axis=uvdistance,amp options=nofqav
-		done
+	# Flag initial extreme values
+	flag_extreme $pcal.$freq 200
 
-		echo -e "\n\e[38;5;166mDo more interactive flagging? \e[38;5;196m(y/n)\e[39m"
-		read flagmore
-		case $flagmore in
-		    [Yy]* ) true ;;
-		    [Nn]* ) break ;;
-		esac
-		
-	    done
-	    
-	    break;;
-	
-	[Nn]* )
-	    echo -e "\e[38;5;166mSkipping flagging\e[39m"
-	    break;;
-    esac
-fi
+	print "Running mfcal on $pcal.$freq"
+	# Solve for initial bandpass on primary calibrator
+	mfcal vis=$pcal.$freq interval=40.0 refant=1
 
-# -----------
-# CALIBRATION
-# -----------
+	# Iterate between RFI flagging and bandpass solution until spectrum is clean
+	while true; do
 
-echo -e "\n\e[38;5;166mProceed with calibration? \e[38;5;196m(y/n)\e[39m"
+	    # Run blflag to clean up lower level RFI 
+	    print "Running blflag on $pcal.$freq"
+	    blflag vis=$pcal.$freq device=/xs stokes=xx,yy,xy,yx axis=time,amp options=nofqav,nobase
+	    blflag vis=$pcal.$freq device=/xs stokes=xx,yy,xy,yx axis=chan,amp options=nofqav,nobase
+	    blflag vis=$pcal.$freq device=/xs stokes=xx,yy,xy,yx axis=uvdistance,amp options=nofqav
+
+	    # Solve for bandpass
+	    print "Running mfcal on $pcal.$freq"
+	    mfcal vis=$pcal.$freq interval=40.0 refant=1
+
+	    # Automatic flagging
+	    prompt "Run automatic flagging?"
+	    read autoflag
+	    case $autoflag in
+		[Yy]* )
+		    print "Running pgflag on $pcal.$freq"
+		    pgflag vis=$pcal.$freq command="<b" device=/xs stokes=xx,yy,xy,yx
+		    print "Running mfcal on $pcal.$freq"
+		    mfcal vis=$pcal.$freq interval=40.0 refant=1
+		    ;;
+		[Nn]* )
+		    : ;;
+	    esac
+
+
+	    prompt "Do more interactive flagging?"
+	    read flagmore
+	    case $flagmore in
+		[Yy]* ) : ;;
+		[Nn]* )
+		    cp -r $pcal.$freq $pcal.$freq.flagged_backup
+		    break ;;
+	    esac
+
+	done ;;
+
+    [Nn]* )
+	print "Skipping flagging" ;;
+esac
+
+prompt "Apply primary gain calibration?"
+read primary_cal
+case $primary_cal in
+    [Yy]* ) 
+
+	# Gain calibrations for primary
+	gpcal vis=$pcal.$freq interval=0.1 options=xyvary minants=3 nfbin=8 spec=2.1 refant=1;
+
+	# Check primary calibration
+	uvplt vis=$pcal.$freq stokes=xx,yy axis=real,imag options=nofqav,nobase,equal device=/xs
+	;;
+
+    [Nn]* )
+	print "Skipping primary gain calibration"
+	;;
+esac
+
+# ----------------------------
+# SECONDARY / GAIN CALIBRATION
+# ----------------------------
+
+# Copy calibration to secondary calibrator
+gpcopy vis=$pcal.$freq out=$scal.$freq options=nocal;
+flag_extreme $scal.$freq 200
+
+prompt "Proceed with secondary flagging?"
 read calibrate
 
 case $calibrate in
 	
 	[Yy]* )
-	    echo -e "\n\e[38;5;166mPerforming gain calbration on primary\e[39m"
-	    # Gain calibrations for primary
-	    for i in 1 2;
-	    do gpcal vis=$pcal.2100.$i interval=0.1 options=xyvary minants=3 nfbin=8 spec=2.1 refant=1;
-	    done
 
-	    # Check primary calibration
-	    for i in 1 2; do
-		uvplt vis=$pcal.2100.$i stokes=xx,yy axis=time,amp options=nofqav device=/xs
-		uvplt vis=$pcal.2100.$i stokes=xx,yy axis=time,phase options=nofqav device=/xs
-		uvplt vis=$pcal.2100.$i stokes=xx,yy axis=real,imag options=nofqav,nobase,equal device=/xs
-	    done
-	    # Copy calibration to secondary calibrator
-	    for i in 1 2;
-	    do gpcopy vis=$pcal.2100.$i out=$scal.2100.$i;
-	    done
-
-	    echo -e "\n\e[38;5;166mFlagging secondary calibrator\e[39m"
+	    print "Flagging secondary calibrator"
 	    # Iterate between gain solutions and RFI flagging
 	    while true; do
 
 		# Run blflag on secondary prior to calibrating
-		for i in 1 2;
-		do echo -e "\n\e[38;5;166mRunning blflag on $scal.2100.$i\e[39m"
-		   blflag vis=$scal.2100.$i device=/xs stokes=xx,yy,xy,yx axis=chan,amp options=nofqav,nobase
-		   blflag vis=$scal.2100.$i device=/xs stokes=xx,yy,xy,yx axis=time,amp options=nofqav
-		   blflag vis=$scal.2100.$i device=/xs stokes=xx,yy,xy,yx axis=uvdistance,amp options=nofqav
-		done
+		print "Running blflag on $scal.$freq"
+		blflag vis=$scal.$freq device=/xs stokes=xx,yy,xy,yx axis=time,amp options=nofqav,nobase
+		blflag vis=$scal.$freq device=/xs stokes=xx,yy,xy,yx axis=chan,amp options=nofqav,nobase
+		blflag vis=$scal.$freq device=/xs stokes=xx,yy,xy,yx axis=chan,amp options=nofqav
+		blflag vis=$scal.$freq device=/xs stokes=xx,yy,xy,yx axis=uvdistance,amp options=nofqav
+
+		# Run automatic flagging on secondary
+		prompt "Run automatic flagging?"
+		read autoflag
+		case $autoflag in
+		    [Yy]* )
+			print "Running pgflag on $scal.$freq"
+			pgflag vis=$scal.$freq command="<b" device=/xs stokes=xx,yy,xy,yx
+			;;
+		    [Nn]* ) : ;;
+		esac
 		
 		# Gain calibrations for secondary calibrator
-		for i in 1 2;
-		do gpcal vis=$scal.2100.1  interval=0.1 options="xyvary,qusolve" minants=3 nfbin=4 refant=1;
-		done
+		gpcal vis=$scal.$freq interval=0.1 options="xyvary,qusolve" minants=3 nfbin=4 refant=1;
 
 		# Check secondary calibration
-		uvplt vis=$scal.2100.1 stokes=xx,yy axis=time,amp options=nofqav device=/xs
-		uvplt vis=$scal.2100.1 stokes=xx,yy axis=time,phase options=nofqav device=/xs
-		uvplt vis=$scal.2100.1 stokes=xx,yy axis=real,imag options=nofqav,nobase,equal device=/xs
+		# uvplt vis=$scal.$freq stokes=xx,yy axis=time,amp options=nofqav device=/xs
+		# uvplt vis=$scal.$freq stokes=xx,yy axis=time,phase options=nofqav device=/xs
+		uvplt vis=$scal.$freq stokes=xx,yy axis=real,imag options=nofqav,nobase,equal device=/xs
 
-		echo -e "\n\e[38;5;166mDo more interactive flagging? \e[38;5;196m(y/n)\e[39m"
+		prompt "Do more interactive flagging?"
 		read flagmore
 		case $flagmore in
-		    [Yy]* ) true ;;
-		    [Nn]* ) break ;;
+		    [Yy]* ) : ;;
+		    [Nn]* )
+			cp -r $scal.$freq $scal.$freq.flagged_backup
+			break ;;
 		esac
 		
-	    done
-
-	    echo -e "\n\e[38;5;166mPropagating flux scale to secondary calibrator\e[39m"
-	    # Transfer flux scale from primary to secondary
-	    for i in 1 2;
-	    do gpboot vis=$scal.2100.$i cal=$pcal.2100.$i;
-	    done
-
-	    echo -e "\n\e[38;5;166mTransferring calibration tables to science target\e[39m"
-	    # Transfer gain calibrations to target
-	    for i in 1 2;
-	    do gpcopy vis=$scal.2100.$i out=$target.2100.$i;
-	    done
-
-	    echo -e "\n\e[38;5;166mAveraging 2 minute calibration samples\e[39m"
-	    # Average gain phase solutions over 2 minutes for better interpolation
-	    for i in 1 2;
-	    do gpaver vis=$target.2100.$i interval=2;
-	    done
-	    
-	    echo -e "\n\e[38;5;166mFlagging science target\e[39m"
-	    while true; do
-		
-		# Run blflag on science target prior to calibrating
-		for i in 1 2;
-		do echo -e "\n\e[38;5;166mRunning blflag on $target.2100.$i\e[39m"
-		   blflag vis=$target.2100.$i device=/xs stokes=xx,yy,xy,yx axis=chan,amp options=nofqav,nobase
-		   blflag vis=$target.2100.$i device=/xs stokes=xx,yy,xy,yx axis=time,amp options=nofqav
-		   blflag vis=$target.2100.$i device=/xs stokes=xx,yy,xy,yx axis=uvdistance,amp options=nofqav
-		done
-
-		# Check target calibration
-		uvplt vis=$target.2100.1 stokes=xx,yy axis=time,amp options=nofqav device=/xs
-		uvplt vis=$target.2100.1 stokes=xx,yy axis=time,phase options=nofqav device=/xs
-		uvplt vis=$target.2100.1 stokes=xx,yy axis=real,imag options=nofqav,nobase,equal device=/xs
-
-		echo -e "\n\e[38;5;166mDo more interactive flagging? \e[38;5;196m(y/n)\e[39m"
-		read flagmore
-		case $flagmore in
-		    [Yy]* ) true ;;
-		    [Nn]* ) break ;;
-		esac
-		
-	    done
-	    
-	    echo -e "\n\e[38;5;166mApplying calibration to science target\e[39m"
-	    # Apply calibrations to $target data
-	    for i in 1 2;
-	    do uvaver vis=$target.2100.$i out=$target.2100.cal.$i;
-	       uvaver vis=$scal.2100.$i out=$scal.2100.cal.$i
-	    done
-
-	    break;;
+	    done ;;
 	
 	[Nn]* )
-	    echo -e "\e[38;5;166mExiting prior to calibration\e[39m"
-	    exit 0
-	    break;;
-    esac
+	    print "Skipping secondary flagging"
+esac
 
+print "Propagating flux scale to secondary calibrator"
+# Transfer flux scale from primary to secondary
+gpboot vis=$scal.$freq cal=$pcal.$freq;
+
+print "Transferring calibration tables to science target"
+# Transfer gain calibrations to target
+gpcopy vis=$scal.$freq out=$target.$freq;
+flag_extreme $target.$freq 20
+
+print "Averaging 2 minute calibration samples"
+# Average gain phase solutions over 2 minutes for better interpolation
+gpaver vis=$target.$freq interval=2;
+
+prompt "Proceed with target flagging?"
+read calibrate
+case $calibrate in
+	
+    [Yy]* )
+
+	print "Flagging science target"
+	while true; do
+
+	    # Run blflag on science target prior to calibrating
+	    print "Running blflag on $target.$freq"
+	    blflag vis=$target.$freq device=/xs stokes=xx,yy,xy,yx axis=time,amp options=nofqav
+	    blflag vis=$target.$freq device=/xs stokes=xx,yy,xy,yx axis=chan,amp options=nofqav
+
+	    # Run automatic flagging on secondary
+	    prompt "Run automatic flagging?"
+	    read autoflag
+	    case $autoflag in
+		[Yy]* )
+		    print "Running pgflag on $target.$freq"
+		    pgflag vis=$target.$freq command="<b" device=/xs stokes=xx,yy,xy,yx
+		    ;;
+		[Nn]* )
+		    : ;;
+	    esac
+
+	    # Check target calibration
+	    # uvplt vis=$target.$freq stokes=xx,yy axis=time,amp options=nofqav device=/xs
+	    # uvplt vis=$target.$freq stokes=xx,yy axis=time,phase options=nofqav device=/xs
+	    uvplt vis=$target.$freq stokes=xx,yy axis=real,imag options=nofqav,nobase,equal device=/xs
+
+	    prompt "Do more interactive flagging?"
+	    read flagmore
+	    case $flagmore in
+		[Yy]* ) : ;;
+		[Nn]* )
+		    cp -r $target.$freq $target.$freq.flagged_backup
+		    break ;;
+	    esac
+	    
+	done
+	;;
+    
+    [Nn]* ) :
+	    ;;
+    
+esac
+
+print "Applying calibration to science target"
+uvaver vis=$scal.$freq out=$scal.$freq.cal
+uvaver vis=$target.$freq out=$target.$freq.cal
+
+# Clean up miriad files before moving to CASA (just keep calibrated target)
+mkdir $proj_dir/miriad
+mv $proj_dir/* $proj_dir/miriad/.
+mv $proj_dir/miriad/$target.$freq.cal $proj_dir/.
+
+print "DONE!"
