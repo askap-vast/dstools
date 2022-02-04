81cef9c3a88064e6c28ce18b391f004bd25569d4
Joshua <jpri6587@sydney.edu.au>
2022-02-04 18:49:08 +1100

    updated image, atca_cal, and avg_baselines scripts

--- avg_baselines.py
+++ avg_baselines.py
@@ -1,16 +1,8 @@
 import numpy as np
 import sys
 
-msname = sys.argv[-2]
-fixphase = sys.argv[-1]
-savename = msname.replace('.ms', '.rotated.target.baseavg.ms')
-rotated_ms = msname.replace('.ms', '.rotated.target.ms')
-
-if fixphase == 'y':
-    # phasecen = "J2000 21h16m06.23s +29d51m56.49s"
-    phasecen = 'J2000 21h09m24.632s -56d18m43.851s'
-    fixvis(vis=msname, outputvis=rotated_ms, phasecenter=phasecen)
-    msname = rotated_ms
+msname = sys.argv[-1]
+savename = msname.replace('.ms', '.baseavg.ms')
 
 intab = tbtool()
 intab.open(msname, nomodify=False)
@@ -27,7 +19,7 @@ interval = intab.getcol('INTERVAL')
 timebin = '{}s'.format(min(interval) * 1e-2)
 
 # average over baselines greater than 200m
-mstransform(vis=msname, outputvis=savename, datacolumn='data', uvrange='>200m',
+mstransform(vis=msname, outputvis=savename, datacolumn='data', uvrange='>500m',
             timeaverage=True, timebin=timebin, keepflags=False)
 
 intab.putcol('ANTENNA1', ant1)
