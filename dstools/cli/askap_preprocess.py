55e9ed97513be8bd140684b2d426ed9257eec67f
Joshua Pritchard <joshua.pritchard@sydney.edu.au>
2023-09-04 11:05:37 +1000

    added ASKAP preprocessing script to fix beam position and flux scale

---
+++ dstools/cli/askap_preprocess.py
@@ -0,0 +1,24 @@
+import os
+import click
+import dstools
+
+@click.command()
+@click.argument('ms')
+def main(ms):
+
+    root = dstools.__path__[0]
+
+    # Fix beam pointing
+    os.system(
+        f'python {root}/fix_dir.py {ms}'
+    )
+    
+    # Convert instrumental pol visibilities from average to total flux
+    os.system(
+        f'python {root}/rescale_askapsoft.py {ms}'
+    )
+
+    return
+
+if __name__ == '__main__':
+    main()
