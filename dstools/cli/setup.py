5dce18e6fe0f76660a5da27c347e406971e3cfca
Joshua Pritchard <joshua.pritchard@sydney.edu.au>
2024-09-01 07:18:00 +1000

    modify setup script to test for DStools installation

--- dstools/cli/setup.py
+++ dstools/cli/setup.py
@@ -1,9 +1,10 @@
-import glob
 import logging
+import os
 import shutil
 import subprocess
 from pathlib import Path
 
+import dstools
 from dstools.logger import setupLogger
 
 logger = logging.getLogger(__name__)
@@ -13,25 +14,47 @@ setupLogger(verbose=False)
 def main():
     casa_bin = shutil.which("casa")
     casa_dir = Path(casa_bin).parent.parent
-    dstools_pkgs = glob.glob(f"{casa_dir}/lib/py/lib/python*/site-packages/*dstools*")
-    dstools_installed = len(dstools_pkgs) > 0
-    if len(dstools_pkgs) > 0:
-        logger.error(f"DStools already installed into CASA environment at {casa_dir}")
-        exit(1)
 
-    cmd = "import sys\nimport subprocess\nsubprocess.check_call([sys.executable, '-m', 'pip', 'install', 'py-dstools'])"
-    call = [
+    # Attempt import of DStools and dependencies to test installation
+    cmd = [
         casa_bin,
         "--nologger",
         "--nologfile",
         "-c",
-        cmd,
+        "import dstools",
+        "import astropy",
+        "import click",
     ]
-    subprocess.run(
-        call,
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.DEVNULL,
-    )
+    out = subprocess.run(cmd, capture_output=True)
+    dstools_installed = "ModuleNotFoundError" not in str(out.stderr)
+
+    if dstools_installed:
+        logger.info(f"DStools already installed into CASA environment at {casa_dir}")
+        exit()
+
+    # Get installation paths of DStools and CASA python distribution
+    cmd = [
+        "casa",
+        "--nologger",
+        "--nologfile",
+        "-c",
+        "import numpy",
+        "print(numpy.__path__[0])",
+    ]
+    out = str(subprocess.run(cmd, capture_output=True).stdout).split(r"\n")
+
+    dstools_python_path = Path(dstools.__path__[0]).parent
+    dstools_casa_path = Path([line for line in out if "numpy" in line][0]).parent
+
+    # Copy DStools and dependencies into CASA python environment
+    packages = [
+        "dstools",
+        "astropy",
+        "click",
+    ]
+    for package in packages:
+        os.system(f"cp -r {dstools_python_path}/{package}* {dstools_casa_path}")
+
     logger.info(f"DStools installed into CASA environment at {casa_dir}")
 
 
