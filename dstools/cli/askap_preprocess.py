13d0a63ab98d24d687f7801a63093cb68ebb56e3
Joshua Pritchard <joshua.pritchard@sydney.edu.au>
2025-02-16 14:20:30 +1100

    delegate ASKAP preprocessing to FixMS

--- dstools/cli/askap_preprocess.py
+++ dstools/cli/askap_preprocess.py
@@ -1,31 +1,36 @@
 import logging
-import os
+from pathlib import Path
 
 import click
 from casacore.tables import tableexists
+from fixms.fix_ms_corrs import fix_ms_corrs
+from fixms.fix_ms_dir import fix_ms_dir
 
-import dstools
-from dstools.logger import setupLogger
+from dstools.logger import filter_stdout, setupLogger
 
 logger = logging.getLogger(__name__)
 setupLogger(verbose=False)
 
 
+@filter_stdout("Successful read/write open of default-locked table")
+def filtered_fixms(ms: Path):
+    # Fix beam pointing
+    fix_ms_dir(str(ms))
+
+    # Convert instrumental pol visibilities from average to total flux
+    fix_ms_corrs(ms, chunksize=250)
+
+    return
+
+
 @click.command(context_settings={"show_default": True})
-@click.argument("ms")
+@click.argument("ms", type=Path)
 def main(ms):
-
-    root = dstools.__path__[0]
-
     if tableexists(f"{ms}/FIELD_OLD"):
         logger.error("ASKAP beam pointing and flux re-scaling already applied.")
         exit(1)
 
-    # Fix beam pointing
-    os.system(f"python {root}/fix_dir.py {ms}")
-
-    # Convert instrumental pol visibilities from average to total flux
-    os.system(f"python {root}/rescale_askapsoft.py {ms}")
+    filtered_fixms(ms)
 
     return
 
