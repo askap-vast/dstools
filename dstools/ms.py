ba374c8d40095901543e4317876fd3a1ad9629f4
Joshua Pritchard <joshua.pritchard@sydney.edu.au>
2025-04-07 16:21:23 +1000

    move selfcal functions to MS module

--- dstools/ms.py
+++ dstools/ms.py
@@ -27,7 +27,7 @@ from dstools.casa import (
     split,
     uvsub,
 )
-from dstools.utils import DataError
+from dstools.utils import DataError, prompt
 
 logger = logging.getLogger(__name__)
 
@@ -453,7 +453,7 @@ class MeasurementSet(Table):
         if not split_ms:
             return self
 
-        subtracted_ms = self.path.with_suffix(".subbed.ms")
+        subtracted_ms = self.path.with_suffix(".subtracted.ms")
         split(
             str(self.path),
             outputvis=str(subtracted_ms),
@@ -573,3 +573,76 @@ def combine_spws(ms: MeasurementSet) -> MeasurementSet:
     )
 
     return MeasurementSet(path=outvis)
+
+
+def run_selfcal(
+    ms: MeasurementSet,
+    calmode: str,
+    gaintype: str,
+    interval: str,
+    split_data: bool,
+    interactive: bool,
+    refant: Optional[str] = None,
+    nspws: int = 1,
+) -> Path:
+    """Perform self-calibration on MS with field model in the MODEL_DATA column."""
+
+    if not re.fullmatch(r"(\d+)(s|min)", interval):
+        raise ValueError(
+            "Argument 'interval' must have format <int>[min/s] (e.g. 10s, 5min)."
+        )
+
+    if not ms.column_exists("MODEL_DATA"):
+        raise DataError(f"{ms} does not contain a MODEL_DATA column.")
+
+    # Produce MS with multiple spectral windows
+    ms.to_nspws(nspws)
+
+    # Solve for self calibration solutions
+    ms.solve_gains(
+        interval,
+        calmode,
+        gaintype,
+        refant=refant,
+        interactive=interactive,
+    )
+
+    # Generate phase and amplitude calibration plots
+    for mode in calmode:
+        ms.caltable.plot_solutions(mode)
+
+    if interactive:
+        plt.show(block=False)
+
+    # Confirm solution is good before applying
+    cal_good = prompt(
+        msg="Apply gain solutions?",
+        bypass=not interactive,
+        default_response=True,
+    )
+
+    # If unacceptable, remove calibration tables, plots, and multi-spw MS and return
+    if not cal_good:
+        os.system(f"rm -r {ms.caltable.path}")
+        os.system(f"rm {ms.path.stem}*.png")
+
+        if nspws > 1:
+            os.system(f"rm -r {ms.path} ")
+            ms.path = ms.original_path
+            ms.original_path = None
+
+        return ms
+
+    # Otherwise proceed with applying calibration solutions
+    ms.applycal()
+
+    # Transform back to single SPW MS
+    ms.to_nspws(1)
+
+    # Split out calibrated MS
+    if split_data:
+        ms = ms.split_selfcal_round()
+
+    plt.close("all")
+
+    return ms
